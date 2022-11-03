8a179631568730a8e2308893e8b8752f7156e788
Mayuresh Rajendra Yadav <69915267+Mayuresh3@users.noreply.github.com>
2022-11-03 21:53:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,21 @@ st.title("Cloud Computing")
 num = st.number_input('Pick a number', 0, 100)
 if(num>90):
     st.write("O")
+    st.balloons()
 elif(num>80):
     st.write("A")
+    st.balloons()
 elif(num>70):
     st.write("B")
+    st.balloons()
 elif(num>60):
     st.write("C")
+    st.snow()
 elif(num>50):
     st.write("D")
+    st.snow()
 elif(num>40):
     st.write("E")
+    st.snow()
 else:
-    st.write("Fail")
+    st.warning("Failed")
